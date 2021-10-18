0c2bd9e8f66ae54c174b02291059a11d2d93d846
Ferris Schwanebeck <ferris.s@aol.de>
2021-10-18 18:24:49 +0200

    tried to fix telegram bot

--- telegram_bot_module.py
+++ telegram_bot_module.py
@@ -1,39 +1,44 @@
+from datetime import datetime, timedelta
 import logging as log
 import time
+from zoneinfo import ZoneInfo
 import telegram
 
 telegram_bot_api_key = "API_KEY"
 group_chat_id = groupChatId
 private_chat_id = privateChatId
 
+de_timezone = ZoneInfo("Europe/Berlin")
 bot = telegram.Bot(token=telegram_bot_api_key)
+next_send_time = datetime.now(de_timezone) - timedelta(seconds=5)
+
+
+def send_message(text: str, chat_id: int, silent: bool = False):
+    global next_send_time
+    error_occurred = False
+    while datetime.now(de_timezone) < next_send_time:
+        time.sleep(1)
+
+    try:
+        bot.send_message(text=text, chat_id=chat_id, disable_notification=silent, disable_web_page_preview=False)
+    except Exception as e:
+        log.error(e)
+        error_occurred = True
+    finally:
+        next_send_time = datetime.now(de_timezone) + timedelta(seconds=5)
+        if error_occurred:
+            return send_message_to_group(text, silent)
 
 
 def send_message_to_group(text: str, silent: bool = False):
-    try:
-        bot.send_message(text=text, chat_id=group_chat_id, disable_notification=silent)
-    except Exception as e:
-        log.error(e)
-        log.error("Try again in 5 seconds")
-        time.sleep(5)
-        return send_message_to_group(text, silent)
+    send_message(text=text, chat_id=group_chat_id, silent=silent)
 
 
 def send_message_to_ferris(text: str, silent: bool = False):
-    try:
-        bot.send_message(text=text, chat_id=private_chat_id, disable_notification=silent)
-    except Exception as e:
-        log.error(e)
-        log.error("Try again in 5 seconds")
-        time.sleep(5)
-        return send_message_to_ferris(text, silent)
+    send_message(text=text, chat_id=private_chat_id, silent=silent)
 
 
 def send_database_to_ferris(relative_db_path: str):
-    try:
-        bot.send_document(chat_id=private_chat_id, document=open(relative_db_path, 'rb'))
-    except Exception as e:
-        log.error(e)
-        log.error("Try again in 5 seconds")
-        time.sleep(5)
-        return send_database_to_ferris(relative_db_path)
+    bot.send_document(chat_id=private_chat_id, document=open(relative_db_path, 'rb'))
+    global next_send_time
+    return send_database_to_ferris(relative_db_path)
