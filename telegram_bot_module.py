2aa19e46f118b04a79d202a34ba699227f7a1dd9
Ferris Schwanebeck <ferris.s@aol.de>
2021-10-18 17:21:24 +0200

    Added exception handling for telegram

--- telegram_bot_module.py
+++ telegram_bot_module.py
@@ -1,4 +1,5 @@
-import logging
+import logging as log
+import time
 import telegram
 
 telegram_bot_api_key = "API_KEY"
@@ -9,12 +10,30 @@ bot = telegram.Bot(token=telegram_bot_api_key)
 
 
 def send_message_to_group(text: str, silent: bool = False):
-    bot.send_message(text=text, chat_id=group_chat_id, disable_notification=silent)
+    try:
+        bot.send_message(text=text, chat_id=group_chat_id, disable_notification=silent)
+    except Exception as e:
+        log.error(e)
+        log.error("Try again in 5 seconds")
+        time.sleep(5)
+        return send_message_to_group(text, silent)
 
 
 def send_message_to_ferris(text: str, silent: bool = False):
-    bot.send_message(text=text, chat_id=private_chat_id, disable_notification=silent)
+    try:
+        bot.send_message(text=text, chat_id=private_chat_id, disable_notification=silent)
+    except Exception as e:
+        log.error(e)
+        log.error("Try again in 5 seconds")
+        time.sleep(5)
+        return send_message_to_group(text, silent)
 
 
-def send_database_to_ferris():
-    bot.send_document(chat_id=private_chat_id, document=open('database/nanopool_mining.db', 'rb'))
+def send_database_to_ferris(relative_db_path: str):
+    try:
+        bot.send_document(chat_id=private_chat_id, document=open(relative_db_path, 'rb'))
+    except Exception as e:
+        log.error(e)
+        log.error("Try again in 5 seconds")
+        time.sleep(5)
+        return send_message_to_group(text, silent)
